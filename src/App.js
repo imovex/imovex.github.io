076c1669e8982debebed38c656cd0bc0c1d1f4d8
Sarah Schulz <sarah.schulz@inovex.de>
2024-05-30 10:21:32 +0200

    404 Umleitung

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, BrowserRouter as Router, Routes, Redirect, useNavigate } from 'react-router-dom';
 import Intro from './Intro';
 import Disclaimer from './Disclaimer';
 import Data from './Data';
@@ -8,22 +8,35 @@ import Schedule from './Schedule';
 import ScheduleGamified from './ScheduleGamified';
 import ThankYou from './ThankYou';
 import Leaderboard from './Leaderboard';
+import NotFoundPage from './NotFoundPage';
 import './App.css';
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const redirectPath = localStorage.getItem('redirectPath');
+    if (redirectPath) {
+      localStorage.removeItem('redirectPath');
+      navigate(redirectPath);
+    }
+  }, [navigate]);
 
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/intro' Component={Intro}></Route>          
-          <Route path='/disclaimer' Component={Disclaimer}></Route>
-          <Route path='/data' Component={Data}></Route>
-          <Route path='/schedule' Component={Schedule}></Route>          
-          <Route path='/gschedule' Component={ScheduleGamified}></Route>          
-          <Route path='/leaderboard' Component={Leaderboard}></Route>
-          <Route path='/settings' Component={Settings}></Route>
-          <Route path='/thankyou' Component={ThankYou}></Route>
+        <Route path='/intro' element={<Intro />} />
+          <Route path='/disclaimer' element={<Disclaimer />} />
+          <Route path='/data' element={<Data />} />
+          <Route path='/schedule' element={<Schedule />} />
+          <Route path='/gschedule' element={<ScheduleGamified />} />
+          <Route path='/leaderboard' element={<Leaderboard />} />
+          <Route path='/settings' element={<Settings />} />
+          <Route path='/thankyou' element={<ThankYou />} />
+          <Route path='/404' element={<NotFoundPage />} />
+          <Route path="*" element={<NotFoundPage />} /> {/* Catch-all route */}
         </Routes>
       </Router>
     </div>
